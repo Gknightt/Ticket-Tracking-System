c3d4f6d462ede15c2212e2d8c94cf3ef30b5cd96
Gknightt <rivovebayo@gmail.com>
2025-06-20 00:48:22 +0800

    celery updated

--- user_service/user_service/celery.py
+++ user_service/user_service/celery.py
@@ -4,5 +4,9 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_service.settings')
 
 app = Celery('user_service')
+
+# Load broker URL from environment variable
+app.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
