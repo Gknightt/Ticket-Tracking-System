58ba321104d149d6f7b1cda2abe965a13e785266
mrccdric27 <cubecore27@gmail.com>
2025-10-03 15:17:38 +0800

    fixes #13
    - has implemented auth service on docker, login works
    - retired auth-service
    - fixes seeding

--- user_service/notifications/urls.py
+++
@@ -1,9 +0,0 @@
-# notifications/urls.py
-
-from django.urls import path
-from .views import NotificationListView, NotificationMarkReadView
-
-urlpatterns = [
-    path('', NotificationListView.as_view(), name='notification-list'),
-    path('<int:pk>/read/', NotificationMarkReadView.as_view(), name='notification-read'),
-]
