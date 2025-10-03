364aa672d7eb353166248e518fffc491e34cc5ec
mrccdric27 <cubecore27@gmail.com>
2025-10-03 15:17:38 +0800

    fixes #13
    - has implemented auth service on docker, login works
    - retired auth-service
    - fixes seeding

--- user_service/accounts/test.py
+++
@@ -1,5 +0,0 @@
-from django.conf import settings
-
-frontend_base_url = settings.FRONTEND_URL
-
-print(frontend_base_url)
--- user_service/role/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', RoleListCreateView.as_view(), name='position-list-create'),
-    path('<int:id>', RoleDetailView.as_view(), name='position-detail-view'),
-]
