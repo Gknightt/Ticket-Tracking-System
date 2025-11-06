e0bac050e4b98fb32e4db5d03c982bf74205b367
mrccdric27 <cubecore27@gmail.com>
2025-11-07 01:39:45 +0800

    feat: add secure step instance retrieval with authorization checks, implement useSecureStepInstance hook, and update AdminTicketDetail component to utilize new functionality

--- workflow_api/step_instance/urls.py
+++ workflow_api/step_instance/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import StepInstanceView
+from .views import StepInstanceView, SecureStepInstanceDetailView
 from .action_views import AvailableActionsView
 from .trigger_views import TriggerNextStepView
 from .simple_views import SimpleStepInstanceView
@@ -8,6 +8,7 @@ urlpatterns = [
     path('<str:step_instance_id>/actions/', AvailableActionsView.as_view()),
     # path('<str:step_instance_id>/', TriggerNextStepView.as_view()),
     path('<uuid:step_instance_id>/', TriggerNextStepView.as_view()),
+    path('secure/<uuid:step_instance_id>/', SecureStepInstanceDetailView.as_view(), name='secure-step-instance-detail'),
     path('list/', StepInstanceView.as_view()),
     path('simple/', SimpleStepInstanceView.as_view()),
 ]
