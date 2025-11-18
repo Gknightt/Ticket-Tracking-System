891eb75423bc1f0f6848ec7d9ebe2d053946e5da
mrccdric27 <cubecore27@gmail.com>
2025-11-18 20:33:11 +0800

    feat: Add HDTS registration shortcut and enhance login/register UI

---
+++ workflow_api/celery.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Start Celery worker for workflow_api
+celery -A workflow_api worker --pool=solo --loglevel=info -Q role_send-default,TICKET_TASKS_PRODUCTION,tts.role.sync,tts.user_system_role.sync,workflow_seed_queue,workflow_seed
